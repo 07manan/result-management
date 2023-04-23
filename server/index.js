bc5a933e6bbf8623c7d283e924d7f88ec1e53c1d
07manan <07manan1850@gmail.com>
2023-04-23 11:18:20 +0530

    dotenv

--- server/index.js
+++ server/index.js
@@ -3,27 +3,22 @@ import mongoose from "mongoose";
 import cors from "cors";
 import resultRoutes from "./routes/Result.js";
 import adminRoutes from "./routes/Adminroutes.js";
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
 
 app.use(express.json());
-app.use('/student', resultRoutes);
-app.use('/admin',adminRoutes);
-
-
-
-const CONNECTION_URL =
-  "mongodb+srv://resultmanagement:resultmanagement123@cluster0.amyk3.mongodb.net/Results?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 5000;
+app.use("/student", resultRoutes);
+app.use("/admin", adminRoutes);
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true })
+  .connect(process.env.CONNECTION_URL, { useNewUrlParser: true })
   .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server running on port: ${PORT}`);
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`Server running on port: ${process.env.PORT || 5000}`);
     });
   })
   .catch((error) => console.log(error));
-  
-
